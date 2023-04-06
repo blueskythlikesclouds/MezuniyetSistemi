b0715975eac9696457549d5d4fb8d2b33a1b44f2
Batuhan Inal <np_batu@hotmail.com>
2023-04-06 22:34:16 +0300

    Base models created

---
+++ Core/Entities/Abstract/DtoBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities.Abstract
+{
+    public abstract class DtoBase : IDto
+    {
+        // Pagination 
+        public virtual int CurrentPage { get; set; } = 1;
+        public virtual int PageSize { get; set; } = 5;
+        public virtual int TotalCount { get; set; }
+        public virtual int TotalPages => (int)Math.Ceiling(decimal.Divide(TotalCount, PageSize));
+        public virtual bool ShowPrevious => CurrentPage > 1;
+        public virtual bool ShowNext => CurrentPage < TotalPages;
+        public virtual bool IsAscending { get; set; } = false;
+    }
+}
---
+++ Core/Entities/Abstract/EntityBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities.Abstract
+{
+    public abstract class EntityBase : IEntity
+    {
+        public virtual int Id { get; set; }
+        public virtual DateTime CreatedDate { get; set; } = DateTime.Now;
+        public virtual DateTime ModifiedDate { get; set; } = DateTime.Now;
+
+        // Hard Delete islemleri disinda
+        public virtual bool IsDeleted { get; set; } = false;
+
+        // Ban - Yasaklama - Aktiflik Durumu kontrolu
+        public virtual bool IsActive { get; set; } = true;
+        public virtual string CreatedByName { get; set; } = "Admin";
+
+        // Guncelleme isleminden sonra ezilmesi gereken alan
+        public virtual string ModifiedByName { get; set; } = "Admin";
+        public virtual string? Note { get; set; } = "Not Girilmemiş";
+    }
+}
