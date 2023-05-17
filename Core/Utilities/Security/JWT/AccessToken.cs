c2141e7763740b7d4047e6430d235048b5432285
Batuhan Inal <np_batu@hotmail.com>
2023-05-17 19:55:03 +0300

    JWT eklendi

---
+++ Core/Utilities/Security/JWT/AccessToken.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Security.JWT
+{
+    public class AccessToken
+    {
+        // Sifrelenmis jwt token degeri
+        public string Token { get; set; }
+        // Gecerlilik tarihi
+        public DateTime Expiration { get; set; }
+    }
+}
